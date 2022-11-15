fbfc29129286f0fc351549f2f3135bcbf6a0e252
GRP4IoT <93915077+GRP4IoT@users.noreply.github.com>
2022-11-15 11:21:23 +0100

    Create vidStreamV0.py

---
+++ pyFiles/math/vidStreamV0.py
@@ -0,0 +1,163 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  	appCamPanTilt.py
+#  	Streaming video with Flask based on tutorial ==> https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+# 	PiCam Local Web Server with PanTilt position Control
+#
+#   MJRoBot.org 30Jan18
+
+import os
+from time import sleep
+from flask import Flask, render_template, request, Response
+#det vi bruger til at tage billeder/knap
+#from picamera.array import PiRGBArray
+#import time
+from picamera import PiCamera
+import datetime
+#from signal import pause
+#from gpiozero import Button
+#import pigpio
+# Raspberry Pi camera module (requires picamera package from Miguel Grinberg)
+from camera_pi import Camera
+
+app = Flask(__name__)
+
+# Global variables definition and initialization
+global panServoAngle
+global tiltServoAngle
+panServoAngle = 90
+tiltServoAngle = 90
+global knapPause
+knapPause = False
+#knap
+#button = Button(4)
+# Servo pin numre
+panPin = 27
+tiltPin = 17
+#camera til at tage billeder:
+#cam = PiCamera()
+#cam.resolution = (1920, 1080)
+#cam.vflip = True
+#cam.contrast = 10
+#cam.framerate = 32
+
+
+@app.route('/')
+def index():
+    #Video streaming home page
+    templateData = {
+      'panServoAngle'	: panServoAngle,
+      'tiltServoAngle'	: tiltServoAngle,
+	  'knapPause'		: str(knapPause)
+	}
+    return render_template('index.html', **templateData)
+
+
+#function til at tage billede flyttet til camera_pi som static method
+#def picture():
+#	date = str(datetime.datetime.now())
+#	file_name = f'/home/g4py/kamera/billeder/pic_{date}.png'
+#	cam.capture(file_name)
+#	print("pic taken")
+
+
+# Inkorperer en knap der tænder og slukker for video stream funktionen
+#flyttet til en anden html side i stedet for tænd/sluk
+def gen(camera):
+	#Video streaming generator function.
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+			b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+	else:
+		print("Test råb")
+
+# Knap funktion der tænder og slukker for video streamen
+# den kommer til at linke til en anden html side der streames til
+@app.route("/start")
+def startKnap():
+#	global knapPause
+#	global panServoAngle
+#	global tiltServoAngle
+#
+#	if 'startstop':
+#		if knapPause == True:
+#			knapPause = False
+#		else:
+#			knapPause = True
+    print("redirecting to stream.html")
+    return render_template('stream.html')
+
+
+# vi skal finde en måde at de-init kamera streamen
+@app.route("/stop")
+def stopKnap():
+    print("sending back to index")
+    templateData = {
+      'panServoAngle'	: panServoAngle,
+      'tiltServoAngle'	: tiltServoAngle,
+	  'knapPause'		: str(knapPause)
+	}
+    return render_template('index.html', **templateData)
+
+
+@app.route('/snap')
+def snapPic():
+	if knapPause == False:
+		Camera.picture()
+		sleep(0.5)
+		print("done.")
+		print("-"*40)
+		print("vi lader som om vi har taget et billede")
+		print("-"*40)
+	else:
+		print("-"*40)
+		print("Du kan ikke tage billede når streamen kører")
+		print("-"*40)
+
+	templateData = {
+      'panServoAngle'	: panServoAngle,
+      'tiltServoAngle'	: tiltServoAngle,
+	  'knapPause'		: str(knapPause)
+	}
+	return render_template('index.html', **templateData)
+
+
+@app.route('/video_feed')
+def video_feed():
+	"""Video streaming route. Put this in the src attribute of an img tag."""
+	return Response(gen(Camera()),
+					mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+@app.route("/<servo>/<angle>")
+def move(servo, angle):
+	global panServoAngle
+	global tiltServoAngle
+	if servo == 'pan':
+		if angle == '+':
+			panServoAngle = panServoAngle + 10
+		else:
+			panServoAngle = panServoAngle - 10
+		os.system("python3 angleServoCtrl.py " + str(panPin) + " " + str(panServoAngle))
+	if servo == 'tilt':
+		if angle == '+':
+			tiltServoAngle = tiltServoAngle + 10
+		else:
+			tiltServoAngle = tiltServoAngle - 10
+		os.system("python3 angleServoCtrl.py " + str(tiltPin) + " " + str(tiltServoAngle))
+
+	templateData = {
+      'panServoAngle'	: panServoAngle,
+      'tiltServoAngle'	: tiltServoAngle,
+	    'knapPause'		: str(knapPause)
+	}
+	return render_template('index.html', **templateData)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
